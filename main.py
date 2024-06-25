0f65a0e2db918470a9da6919a708509771d528b4
DeAleon <donnotel@mail.ru>
2024-06-25 16:38:34 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+# Задача «Длина слова».
+a = 'Welcom Uban Universitet'
+print(len(a))
+
+# Задача «Суммы и разности»
+first = 27
+second = 13
+summa = (first + second)
+diff = (first - second)
+print(summa, diff)
+
+# Задача «Среднее арифметическое»
+first = 31
+second = 21
+third = 11
+mean = ((first + second + third) / 3)
+print(mean)
+
+# Задача «Простые строчки»
+first_string = 'Вторник'
+second_string = 'Понедельник'
+print(second_string + ', ' + first_string)
+
+# Задача «Сложная формула»
+a = 11
+b = 7
+c = 31
+f = (a * b) + (a * c)
+print(f ** 3 / 2)
